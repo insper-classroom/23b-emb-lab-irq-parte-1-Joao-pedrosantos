f6da90ef3586a2ad64acae38ff772ea3661f61a6
João Pedro Rodrigues dos Santos <81189216+Joao-pedrosantos@users.noreply.github.com>
2023-09-03 17:09:19 -0300

    ??

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -4,6 +4,116 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
+
+
+// LED
+#define LED_PIO      PIOC
+#define LED_PIO_ID   ID_PIOC
+#define LED_IDX      8
+#define LED_IDX_MASK (1 << LED_IDX)
+
+// Botão
+#define BUT_PIO      PIOA
+#define BUT_PIO_ID   ID_PIOA
+#define BUT_IDX  11
+#define BUT_IDX_MASK (1 << BUT_IDX)
+
+#define BUT1_PIO	PIOD
+#define BUT1_PIO_ID	 ID_PIOD
+#define BUT1_PIO_IDX 28
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+
+/************************************************************************/
+/* constants                                                            */
+/************************************************************************/
+
+/************************************************************************/
+/* variaveis globais                                                    */
+/************************************************************************/
+volatile char but_flag = 0;
+volatile int tempo = 0;
+/************************************************************************/
+/* prototype                                                            */
+/************************************************************************/
+void io_init(void);
+void pisca_led(int n, int t);
+
+/************************************************************************/
+/* functions                                                            */
+/************************************************************************/
+
+void but_ex(void){
+	int timeout = 100;
+	int button_down_current = 0;
+	int button_down_threshold = 15000000;
+
+	while(!pio_get(BUT_PIO, PIO_INPUT, BUT_IDX_MASK)) {
+		button_down_current++;
+	}
+
+	if (button_down_current >= button_down_threshold) {
+		tempo += 1000;
+	}
+	else if (tempo > timeout) {
+		tempo -= timeout;
+	}
+
+	but_flag = 1;
+}
+
+// pisca led N vez no periodo T
+void pisca_led(int n, int t){
+	pio_set(LED_PIO, LED_IDX_MASK);
+	delay_ms(t);
+	pio_clear(LED_PIO, LED_IDX_MASK);
+	delay_ms(t);
+}
+
+// Inicializa botao SW0 do kit com interrupcao
+void io_init(void)
+{
+	sysclk_init();
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	// Configura led
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	//pmc_enable_periph_clk(BUT2_PIO_ID);
+	//pmc_enable_periph_clk(BUT3_PIO_ID);
+	
+	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+
+	// Inicializa clock do periférico PIO responsavel pelo botao
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+
+	// Configura PIO para lidar com o pino do botão como entrada
+	// com pull-up
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT1_PIO, BUT1_PIO_IDX_MASK, 60);
+
+	// Configura interrupção no pino referente ao botao e associa
+	// função de callback caso uma interrupção for gerada
+	// a função de callback é a: but_callback()
+	pio_handler_set(BUT1_PIO,
+					BUT1_PIO_ID,
+					BUT1_PIO_IDX_MASK,
+					PIO_IT_EDGE,
+					but_ex);
+	
+
+
+	// Ativa interrupção e limpa primeira IRQ gerada na ativacao
+	pio_enable_interrupt(BUT1_PIO, BUT1_IDX_MASK);
+	pio_get_interrupt_status(BUT1_PIO);
+	
+	// Configura NVIC para receber interrupcoes do PIO do botao
+	// com prioridade 4 (quanto mais próximo de 0 maior)
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 4); // Prioridade 4
+}
+
 int main (void)
 {
 	board_init();
@@ -15,7 +125,7 @@ int main (void)
   
   
 	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-  gfx_mono_draw_string("mundo", 50,16, &sysfont);
+    gfx_mono_draw_string("mundo", 50,16, &sysfont);
   
   
 
