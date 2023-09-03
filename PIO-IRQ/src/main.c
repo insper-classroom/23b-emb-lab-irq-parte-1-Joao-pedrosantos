785a876214232463c2ad44c04fb7a92a59b08c55
João Pedro Rodrigues dos Santos <81189216+Joao-pedrosantos@users.noreply.github.com>
2023-09-03 16:08:07 -0300

    aumenta/dimunui freq

--- PIO-IRQ/src/main.c
+++ PIO-IRQ/src/main.c
@@ -38,6 +38,22 @@
 #define BUT_IDX  11
 #define BUT_IDX_MASK (1 << BUT_IDX)
 
+#define BUT1_PIO	PIOD
+#define BUT1_PIO_ID	 ID_PIOD
+#define BUT1_PIO_IDX 28
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+#define BUT2_PIO PIOC
+#define BUT2_PIO_ID ID_PIOC
+#define BUT2_PIO_IDX 31
+#define BUT2_PIO_IDX_MASK (1u << BUT2_PIO_IDX)
+
+#define BUT3_PIO PIOA
+#define BUT3_PIO_ID ID_PIOA
+#define BUT3_PIO_IDX 19
+#define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
+
+
 /************************************************************************/
 /* constants                                                            */
 /************************************************************************/
@@ -45,7 +61,8 @@
 /************************************************************************/
 /* variaveis globais                                                    */
 /************************************************************************/
-
+volatile char but_flag = 0;
+volatile int tempo = 0;
 /************************************************************************/
 /* prototype                                                            */
 /************************************************************************/
@@ -65,7 +82,26 @@ void pisca_led(int n, int t);
  */
 void but_callback(void)
 {
-  pisca_led(5, 200);
+  but_flag = 1;
+}
+
+void but_ex(void){
+	int timeout = 100;
+	int button_down_current = 0;
+	int button_down_threshold = 15000000;
+
+	while(!pio_get(BUT_PIO, PIO_INPUT, BUT_IDX_MASK)) {
+		button_down_current++;
+	}
+
+	if (button_down_current >= button_down_threshold) {
+		tempo += 1000;
+	}
+	else if (tempo > timeout) {
+		tempo -= timeout;
+	}
+
+	but_flag = 1;
 }
 
 /************************************************************************/
@@ -74,28 +110,34 @@ void but_callback(void)
 
 // pisca led N vez no periodo T
 void pisca_led(int n, int t){
-  for (int i=0;i<n;i++){
-    pio_clear(LED_PIO, LED_IDX_MASK);
-    delay_ms(t);
-    pio_set(LED_PIO, LED_IDX_MASK);
-    delay_ms(t);
-  }
+	pio_set(LED_PIO, LED_IDX_MASK);
+	delay_ms(t);
+	pio_clear(LED_PIO, LED_IDX_MASK);
+	delay_ms(t);
 }
 
 // Inicializa botao SW0 do kit com interrupcao
 void io_init(void)
 {
-
+	sysclk_init();
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
   // Configura led
 	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	
 	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
 
   // Inicializa clock do periférico PIO responsavel pelo botao
 	pmc_enable_periph_clk(BUT_PIO_ID);
 
   // Configura PIO para lidar com o pino do botão como entrada
   // com pull-up
-	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT_PIO, BUT_IDX_MASK, 60);
 
   // Configura interrupção no pino referente ao botao e associa
   // função de callback caso uma interrupção for gerada
@@ -103,8 +145,14 @@ void io_init(void)
   pio_handler_set(BUT_PIO,
                   BUT_PIO_ID,
                   BUT_IDX_MASK,
-                  PIO_IT_FALL_EDGE,
-                  but_callback);
+                  PIO_IT_EDGE,
+                  but_ex);
+				  
+   //pio_handler_set(BUT1_PIO,
+				   //BUT1_PIO_ID,
+				   //BUT1_PIO_IDX_MASK,
+				   //PIO_IT_EDGE,
+				   //but_callback);
 
   // Ativa interrupção e limpa primeira IRQ gerada na ativacao
   pio_enable_interrupt(BUT_PIO, BUT_IDX_MASK);
@@ -123,18 +171,19 @@ void io_init(void)
 // Funcao principal chamada na inicalizacao do uC.
 void main(void)
 {
-	// Inicializa clock
-	sysclk_init();
+	// configura botao com interrupcao
+	io_init();
 
-	// Desativa watchdog
-	WDT->WDT_MR = WDT_MR_WDDIS;
-
-  // configura botao com interrupcao
-  io_init();
+	but_flag = 0;
+	tempo = 1000;
 
 	// super loop
 	// aplicacoes embarcadas no devem sair do while(1).
-	while(1)
-  {
+	while(1) {
+	  if (but_flag && tempo > 0) {  // (2)
+			pisca_led(30, tempo);
+			//but_flag = 0;
+	  }
+	  //pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 	}
 }
