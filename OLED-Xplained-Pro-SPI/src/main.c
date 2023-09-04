1cbf7e108cd725f0322730bbbb8a75255ce61764
João Pedro Rodrigues dos Santos <81189216+Joao-pedrosantos@users.noreply.github.com>
2023-09-03 23:29:28 -0300

    funcionando agora

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -33,32 +33,23 @@
 /************************************************************************/
 volatile char but_flag = 0;
 volatile int tempo = 0;
+char buffer[128];
+bool apertado = false;
+int button_down_current = 0;
+
 /************************************************************************/
 /* prototype                                                            */
 /************************************************************************/
 void io_init(void);
 void pisca_led(int n, int t);
+void atualiza_frequencia();
 
 /************************************************************************/
 /* functions                                                            */
 /************************************************************************/
 
-void but_ex(void){
-	int timeout = 100;
-	int button_down_current = 0;
-	int button_down_threshold = 15000000;
-
-	while(!pio_get(BUT_PIO, PIO_INPUT, BUT_IDX_MASK)) {
-		button_down_current++;
-	}
-
-	if (button_down_current >= button_down_threshold) {
-		tempo += 1000;
-	}
-	else if (tempo > timeout) {
-		tempo -= timeout;
-	}
-
+void but_raise(void) {
+	apertado = !apertado;
 	but_flag = 1;
 }
 
@@ -70,12 +61,22 @@ void pisca_led(int n, int t){
 	delay_ms(t);
 }
 
+
+void atualiza_frequencia() {
+	snprintf(buffer, sizeof(buffer) / sizeof(char), "%f", ((float)tempo / (float)1000));
+}
+
 // Inicializa botao SW0 do kit com interrupcao
 void io_init(void)
 {
+	board_init();
 	sysclk_init();
+	delay_init();
 	WDT->WDT_MR = WDT_MR_WDDIS;
-	
+
+	// Init OLED
+	gfx_mono_ssd1306_init();
+
 	// Configura led
 	pmc_enable_periph_clk(LED_PIO_ID);
 	pmc_enable_periph_clk(BUT1_PIO_ID);
@@ -96,16 +97,23 @@ void io_init(void)
 	// Configura interrupção no pino referente ao botao e associa
 	// função de callback caso uma interrupção for gerada
 	// a função de callback é a: but_callback()
-	pio_handler_set(BUT1_PIO,
-					BUT1_PIO_ID,
-					BUT1_PIO_IDX_MASK,
-					PIO_IT_EDGE,
-					but_ex);
-	
+	//pio_handler_set(
+		//BUT1_PIO,
+		//BUT1_PIO_ID,
+		//BUT1_PIO_IDX_MASK,
+		//PIO_IT_FALL_EDGE,
+		//but_fall);
+					
+	pio_handler_set(
+		BUT1_PIO,
+		BUT1_PIO_ID,
+		BUT1_PIO_IDX_MASK,
+		PIO_IT_EDGE,
+		but_raise);
 
 
 	// Ativa interrupção e limpa primeira IRQ gerada na ativacao
-	pio_enable_interrupt(BUT1_PIO, BUT1_IDX_MASK);
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_IDX_MASK);
 	pio_get_interrupt_status(BUT1_PIO);
 	
 	// Configura NVIC para receber interrupcoes do PIO do botao
@@ -114,42 +122,37 @@ void io_init(void)
 	NVIC_SetPriority(BUT1_PIO_ID, 4); // Prioridade 4
 }
 
-int main (void)
-{
-	board_init();
-	sysclk_init();
-	delay_init();
+int main(void) {
+	io_init();
+	but_flag = 0;
+	tempo = 1000;
+	buffer[0] = '\0';
 
-  // Init OLED
-	gfx_mono_ssd1306_init();
-  
-  
-	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-    gfx_mono_draw_string("mundo", 50,16, &sysfont);
-  
-  
-
-  /* Insert application code here, after the board has been initialized. */
-	while(1) {
-
-
-
-			// Escreve na tela um circulo e um texto
-			
-			for(int i=70;i<=120;i+=2){
-				
-				gfx_mono_draw_rect(i, 5, 2, 10, GFX_PIXEL_SET);
-				delay_ms(10);
-				
+int n = 0;
+	while (1) {
+		if (apertado) {
+			button_down_current++;
+		}
+		else {
+			if (button_down_current > 0){
+				if (button_down_current >= 3){
+					tempo -= 100;
+				}
+				else{
+					tempo += 100;
+				}
+				sprintf(buffer, "%d %d %d ", button_down_current, apertado, tempo);
+				gfx_mono_draw_string(buffer, 0, 16, &sysfont);
+				button_down_current = 0;
 			}
-			
-			for(int i=120;i>=70;i-=2){
-				
-				gfx_mono_draw_rect(i, 5, 2, 10, GFX_PIXEL_CLR);
-				delay_ms(10);
-				
-			}
-			
-			
+		}
+		if(but_flag){
+			pisca_led(30, tempo);
+		}
+
+		// Handle LED blinking and other tasks here
+
+		//pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+		//gfx_mono_draw_string(buffer, 50, 16, &sysfont);
 	}
 }
